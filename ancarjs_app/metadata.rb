2b374d097093e3b711a10ee5ce31fc7082822753
t.miki <miki@stadium-m1.com>
2015-10-28 02:59:40 +0900

    説明追加

--- ancarjs_app/metadata.rb
+++ ancarjs_app/metadata.rb
@@ -3,3 +3,6 @@ description "Installs ancarJS application"
 maintainer  "Takafumi Miki"
 
 depends "deploy"
+
+recipe "ancarjs_app::setup", "Setup middlewares for ancarJS"
+recipe "ancarjs_app::deploy", "Deploy ancarJS"
